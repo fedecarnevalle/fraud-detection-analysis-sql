8328807101318fecf808446d2775148f64abb1a2
Federico Carnevalle <151556909+fedecarnevalle@users.noreply.github.com>
2026-03-17 15:03:03 +0000

    Enhance SQL query with comments and formatting
    Added comments to clarify the purpose of the query and improved formatting.

--- queries/01_general_exposure.sql
+++ queries/01_general_exposure.sql
@@ -1,15 +1,20 @@
-WITH initial_metrics AS(
-SELECT
-      CASE WHEN is_fraud = 0 THEN 'Legitimate' ELSE 'Fraud' END AS transaction_type,
-      COUNT(*) AS transactions,
-      ROUND(SUM(amt),1) AS amount
- FROM `fraud-analysis.fraud_credit_card_transactions.fraud_transactions` 
- GROUP BY is_fraud
+-- ================================================================
+-- General Exposure Analysis
+-- Objective: Quantify the overall scale and financial impact of fraud
+-- ================================================================
+
+WITH initial_metrics AS (
+    SELECT
+        CASE WHEN is_fraud = 0 THEN 'Legitimate' ELSE 'Fraud' END AS transaction_type,
+        COUNT(*) AS transactions,
+        ROUND(SUM(amt), 1) AS amount
+    FROM `fraud-analysis.fraud_credit_card_transactions.fraud_transactions`
+    GROUP BY is_fraud
 )
 SELECT 
-      transaction_type,
-      transactions,
-      ROUND(100*(transactions / SUM(transactions) OVER()),1) AS pct_transactions,
-      amount,
-      ROUND(100*(amount / SUM(amount) OVER()),1) AS pct_amount
+    transaction_type,
+    transactions,
+    ROUND(100 * (transactions / SUM(transactions) OVER()), 1) AS pct_transactions,
+    amount,
+    ROUND(100 * (amount / SUM(amount) OVER()), 1) AS pct_amount
 FROM initial_metrics
