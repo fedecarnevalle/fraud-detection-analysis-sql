624391cc5dd38115340980a58fa363bbf7185ce3
Federico Carnevalle <151556909+fedecarnevalle@users.noreply.github.com>
2026-03-17 14:54:36 +0000

    initial commit

---
+++ queries/01_general_exposure.sql
@@ -0,0 +1,15 @@
+WITH initial_metrics AS(
+SELECT
+      CASE WHEN is_fraud = 0 THEN 'Legitimate' ELSE 'Fraud' END AS transaction_type,
+      COUNT(*) AS transactions,
+      ROUND(SUM(amt),1) AS amount
+ FROM `fraud-analysis.fraud_credit_card_transactions.fraud_transactions` 
+ GROUP BY is_fraud
+)
+SELECT 
+      transaction_type,
+      transactions,
+      ROUND(100*(transactions / SUM(transactions) OVER()),1) AS pct_transactions,
+      amount,
+      ROUND(100*(amount / SUM(amount) OVER()),1) AS pct_amount
+FROM initial_metrics
